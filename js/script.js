b6869cd5862e09c8a40f1205f3b906e8ba8058cb
yvital <you@zztop.local>
2019-08-13 11:10:22 +0300

    js commit

--- js/script.js
+++ js/script.js
@@ -1,46 +1,26 @@
 window.onload = function(){
-	var width = window.innerWidth;
-	var height = window.innerHeight;
-	var canvas = document.getElementById('canvas');
-	
-	canvas.setAttribute('width',width);
-	canvas.setAttribute('height',height);
+	var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera( 70, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-var camera, scene, renderer;
-var geometry, material, mesh;
-
-init();
-animate();
-
-function init() {
-
-	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 1;
-
-	scene = new THREE.Scene();
-
-	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-	material = new THREE.MeshNormalMaterial();
-
-	mesh = new THREE.Mesh( geometry, material );
-	scene.add( mesh );
-
-	renderer = new THREE.WebGLRenderer( { canvas: canvas, antialias: true } );
+	var renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
 
-}
+	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	material = new THREE.MeshNormalMaterial();
+	var cube = new THREE.Mesh( geometry, material );
+	scene.add( cube );
 
-function animate() {
+	camera.position.z = 5;
 
+	var animate = function () {
 	requestAnimationFrame( animate );
 
-	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.02;
+		cube.rotation.x += 0.01;
+		cube.rotation.y += 0.01;
+		renderer.render( scene, camera );
+	};
 
-	renderer.render( scene, camera );
-
-}
-
+	animate();
 
 }	
